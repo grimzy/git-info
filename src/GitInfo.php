3f147a361786cbd180e28a6588db133dc576828f
Joseph Estefane <estefanejoe@gmail.com>
2019-10-18 20:39:20 -0400

    Split GitInfo::getInfo():  - Split getInfo() in multiple less complex private methods  - Fixed some types in inline docs

--- src/GitInfo.php
+++ src/GitInfo.php
@@ -19,7 +19,7 @@ class GitInfo
     protected $path;
 
     /**
-     * Array holding all registered commands.
+     * Associative array of <command-name, command> holding all registered commands.
      * Starts with predefined set of commands.
      *
      * @var string[]
@@ -42,7 +42,7 @@ class GitInfo
     /**
      * GitInfo constructor.
      *
-     * @param string|null $path     The current working directory.
+     * @param string|null $path     The working directory. Where we run the commands.
      *                              Defaults to getcwd()
      * @param string[]    $commands List of commands to add to
      *                              GitInfo::$registeredCommands
@@ -53,14 +53,14 @@ class GitInfo
 
         // Register commands provided.
         if (!empty($commands)) {
-            foreach ($commands as $commandName => $command) {
-                self::addCommand($commandName, $command);
+            foreach ($commands as $name => $command) {
+                self::addCommand($name, $command);
             }
         }
     }
 
     /**
-     * Add custom commands.
+     * Add a custom command to $registeredCommands for later use.
      *
      * @param string $name    The command's name
      * @param string $command The command
@@ -70,44 +70,6 @@ class GitInfo
     public static function addCommand(string $name, $command)
     {
         self::$registeredCommands[$name] = $command;
-    }
-
-    /**
-     * This method runs the commands and return the results.
-     *
-     * @param string|string[]|null $commands Command or list of commands to run
-     *                                       If null, runs all registered commands
-     *
-     * @return string|array
-     * @throws \Exception
-     */
-    public function getInfo($commands = null)
-    {
-        $cwd = getcwd();
-        chdir($this->getWorkingDirectory());
-
-        $commandResult = [];
-        // Only continue if we received any commands.
-        if (!empty($commands)) {
-            if (is_array($commands)) {
-                foreach ($commands as $command) {
-                    // Execute the command and save the result to results.
-                    $commandResult[$command] = $this->executeCommand($command);
-                }
-            } elseif (is_string($commands)) {
-                return $this->executeCommand($commands);
-            }
-            return $commandResult;
-        }
-
-        // Execute all the commands registered.
-        foreach (self::$registeredCommands as $commandKey => $command) {
-            $commandResult[$command] = $this->executeCommand($commandKey);
-        }
-
-        chdir($cwd);
-
-        return $commandResult;
     }
 
     /**
@@ -121,6 +83,28 @@ class GitInfo
     }
 
     /**
+     * Run the command(s) provided, and returns the results.
+     * If none are provided, runs all registered commands.
+     *
+     * @param string|string[]|null $names Command name or list of command
+     *                                    names
+     *
+     * @return string|string[]|null
+     * @throws \Exception
+     */
+    public function getInfo($names = null)
+    {
+        $cwd = getcwd();
+        chdir($this->getWorkingDirectory());
+
+        $commandResult = $this->executeAllCommands($names);
+
+        chdir($cwd);
+
+        return $commandResult;
+    }
+
+    /**
      * Retrieve the working directory.
      *
      * @return string
@@ -131,18 +115,75 @@ class GitInfo
     }
 
     /**
-     * This method executes the given command and returns the result.
+     * Executes all commands provided in $names.
+     * If no command name is provided, then run all registered commands.
+     *
+     * @param string|string[]|null $names Command name or list of
+     *                                    commands names to run
+     *
+     * @return string|string[]|null
+     * @throws Exception
+     */
+    private function executeAllCommands($names = null)
+    {
+        if (empty($names)) {
+            return $this->executeRegisteredCommands();
+        }
+
+        if (is_array($names)) {
+            return $this->executeCommands($names);
+        } elseif (is_string($names)) {
+            return $this->executeCommand($names);
+        }
+
+        return null;
+    }
+
+    /**
+     * Executes all registered commands.
+     *
+     * @return string[]
+     * @throws Exception
+     */
+    private function executeRegisteredCommands()
+    {
+        $commandResult = [];
+        foreach (self::$registeredCommands as $name => $command) {
+            $commandResult[$command] = $this->executeCommand($name);
+        }
+        return $commandResult;
+    }
+
+    /**
+     * Executes all commands provided in $names.
+     *
+     * @param string[] $names List of commands names
+     *
+     * @return string[]
+     * @throws Exception
+     */
+    private function executeCommands(array $names)
+    {
+        $commandResult = [];
+        foreach ($names as $name) {
+            $commandResult[$name] = $this->executeCommand($name);
+        }
+        return $commandResult;
+    }
+
+    /**
+     * Execute the given command and returns the result.
      *
      * @param string $name The name of the command to execute
      *
-     * @return mixed
+     * @return string|string[]
      * @throws \Exception
      */
     private function executeCommand($name)
     {
         if (array_key_exists($name, self::$registeredCommands)) {
-            exec(self::$registeredCommands[$name], $result);
-            return $this->commandResultString($result);
+            exec(self::$registeredCommands[$name], $results);
+            return $this->processCommandResults($results);
         }
         throw new Exception('Command: ' . $name . ' not registered.');
     }
@@ -151,17 +192,15 @@ class GitInfo
      * This method returns the results from a command.
      * When $result has only one line, return as string.
      *
-     * @param string[] $result The results from running a command
+     * @param string[] $results The results from running a command
      *
-     * @return string|array
+     * @return string|string[]
      */
-    private function commandResultString(array $result)
+    private function processCommandResults(array $results)
     {
-        if (is_array($result)) {
-            if (count($result) === 1) {
-                return $result[0];
-            }
-            return $result;
+        if (count($results) === 1) {
+            return $results[0];
         }
+        return $results;
     }
 }
